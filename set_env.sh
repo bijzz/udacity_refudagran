4a59d22a3e3ac7c97a7ef75a6d5d76c040cc0810
kern <no@mail.com>
2020-08-18 21:38:21 +0200

    Init

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=12345678#
+export POSTGRES_HOST=db.cgef3h2i7elv.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=refactor-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+#export URL=http://localhost:8100
+export URL=http://localhost:80
